682099d0b32c6b816cb005a0d885d0afc94881be
Harsh Rajput <dev.harsh9730@gmail.com>
2025-08-05 13:55:28 +0530

    🚀 Deploy update: Flutter Web rebuild

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6d7007349adf121a6fd4189658e2c56f",
+const RESOURCES = {"flutter_bootstrap.js": "a36f9f6aeef48d499a1b0826c54a590b",
 "version.json": "d0777a92ee055b57247718ea0074f3c5",
 "index.html": "3c48ff7806695b107d4450f3ed74f17d",
 "/": "3c48ff7806695b107d4450f3ed74f17d",
